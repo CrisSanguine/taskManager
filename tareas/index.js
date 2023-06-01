8a4d4d7f1b851ec0864bb1d671c7e211c31305a1
LudmilaTevez13 <tevez.ludmila13@gmail.com>
2023-06-01 16:59:57 -0300

    changes listar

--- tareas/index.js
+++ tareas/index.js
@@ -1,8 +1,8 @@
-// const { leerJSON, escribirJSON } = require("../data");
+ const { leerJSON, escribirJSON } = require("../data");
 // const Tarea = require("./Tarea");
 
 module.exports = {
-// tareas: leerJSON(),
+ tareas: leerJSON(),
  listar: require('./listar'),
  agregar: require('./agregar'),
 // filtrar: require('./filtrar'),
